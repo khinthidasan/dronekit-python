c253bf0a06b0e6d68f7cb6874c1bba8610daf98f
Khin Thida San <khinthidasan.pont@gmail.com>
2018-05-16 12:34:16 +0900

    Update test.py

--- examples/vehicle_state/test.py
+++ examples/vehicle_state/test.py
@@ -1,5 +1,5 @@
 from dronekit import connect, VehicleMode
-
+print " Khin"
 # Connect to UDP endpoint.
 vehicle = connect('127.0.0.1:14550', wait_ready=True)
 # Use returned Vehicle object to query device state - e.g. to get the mode:
